c9bab0f61f6a670e55e451b1c397905caaf1548b
Tuichan <178974617+Tuichan@users.noreply.github.com>
2024-09-08 01:23:23 +0900

    Update pyxel.py

--- game/pyxel.py
+++ game/pyxel.py
@@ -3,7 +3,7 @@ import pyxel
 pyxel.init(128, 128)
 
 # ビットマップフォントの読み込み
-font = pyxel.Font("umplus_j10r.bdf")
+font = pyxel.Font("umplus_j12r.bdf")
 
 # フォントを指定してテキスト表示
 pyxel.text(10,10,"日本語の表示",7,font)
